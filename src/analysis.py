a4edd2e598394b428eaf7182c1e11147558a8e04
PripatOnGit <patil.priyu26@gmail.com>
2023-12-21 17:22:08 +0530

    python script to load  non-duplicate data from CSV file from data set  to tables in Postgresql database.

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,7 @@
-#import pandas;
+
 import psycopg2;
 from psycopg2 import sql;
+import csv;
 
 database_params = {
     "host": "localhost",
@@ -33,26 +34,55 @@ def execute_query(connection, query):
     except (Exception, psycopg2.Error) as error:
         print("Error executing query:", error)
         return None
+    
+
+
+def insert_data(connection, table_name, data):
+    unique_keys = set()
+    try:
+        with connection.cursor() as cursor:
+            # Construct the INSERT INTO query using sql.SQL
+            query = sql.SQL("INSERT INTO silver.{} (video_id,title ,publishedAt,channelId,channelTitle, categoryId,trending_date,tags,view_count,likes,dislikes,comment_count,thumbnail_link,comments_disabled,ratings_disabled,description) VALUES (%s, %s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);").format(sql.Identifier(table_name))
+
+            # Execute the query for each row of data
+            for row in data:
+                key = row[0]
+                if key not in unique_keys:
+                    cursor.execute(query, row)
+                    unique_keys.add(key)
+                else:
+                    print(f"Skipping duplicate keys: {key}")
+
+        connection.commit()
+        print("Data inserted successfully")
+    except (Exception, psycopg2.Error) as error:
+        print("Error inserting data:", error)
+
+def read_csv(file_path, encoding='utf-8'):
+    with open(file_path, 'r', newline='', encoding=encoding) as csvfile:
+        reader = csv.reader(csvfile)
+        # Skip the header if it exists
+        next(reader, None)
+        return [row for row in reader]
+    
 
 if __name__ == "__main__":
-    #csv filr path
-    file_path = 'D:\Priyanka\Data Engineering\Youtube-trend-analysis\source_data\IN_youtube_trending_data.csv'
     
-    # Example query to select data from a table
-    select_query = sql.SQL("SELECT * FROM silver.youtube_videos;")
+    # Replace 'your_table' and 'your_data.csv' with actual values
+    table_name = 'youtube_videos'
+    csv_file_path = 'D:\Priyanka\Data Engineering\Youtube-trend-analysis\source_data\IN_youtube_trending_data.csv'
 
     # Establish a connection
     connection = create_connection()
 
-    # Execute the query
     if connection:
-        result = execute_query(connection, select_query)
-        if result:
-            print("Query result:", result)
+        # Read data from the CSV file
+        csv_data = read_csv(csv_file_path, encoding='utf-8')
 
-    # Close the connection
-    close_connection(connection)
+        # Insert data into the PostgreSQL database
+        insert_data(connection, table_name, csv_data)
+
+        # Close the connection
+        close_connection(connection)
 
 
-#df = pandas.read_csv('D:\Priyanka\Data Engineering\Youtube-trend-analysis\source_data\IN_youtube_trending_data.csv')
-#print(df.dtypes)
